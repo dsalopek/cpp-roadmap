1a8ddeaf66e27d0c9c3c91bedf67946db8a0c0be
Dylan <dylan.salopek@gmail.com>
2025-10-05 22:42:34 -0500

    week 7 added test support!

--- week-7/test/SensorTest.cpp
+++ week-7/test/SensorTest.cpp
@@ -1,11 +1,26 @@
 #include <gtest/gtest.h>
 
 #include "../include/Sensor.h"
+#include <string>
+#include <cstdint>
 
 // Demonstrate some basic assertions.
 TEST(SensorTest, SensorCreation) {
-    // Expect equality.
-    Sensors::SensorMetadata sensor_metadata = Sensors::sensorMetadata[0];
-    Sensors::Sensor         sensor{"t1", sensor_metadata, 100};
-    EXPECT_EQ(100, sensor.sensorValue()) << "Sensor value is not expected";
+    const std::string                 sensorName{"t1"};
+    constexpr double                  sensorValue{100};
+    constexpr std::uint8_t            defaultSensorState{0b00001111};
+    constexpr Sensors::SensorMetadata sensorMetadata = Sensors::sensorMetadata[
+        0];
+
+    const Sensors::Sensor sensor{sensorName, sensorMetadata, sensorValue};
+    EXPECT_EQ(sensorName, sensor.sensorName()) << "Sensor name is not expected";
+    EXPECT_EQ(sensorMetadata.sensorType,
+              sensor.sensorMetadata().
+              sensorType) << "Sensor type is not expected";
+    EXPECT_EQ(sensorValue,
+              sensor.sensorValue()) << "Sensor value is not expected";
+    EXPECT_FALSE(sensor.isSensorInBadState()) << "Sensor is in bad state";
+    EXPECT_TRUE(sensor.isEnabled()) << "Sensor is not enabled";
+    EXPECT_EQ(defaultSensorState,
+              sensor.sensorStatus()) << "Sensor status is not expected";
 }
